e8243ea55f925bf234c8c41c90f72174af54f0ee
AntonBrevnov <brevnov-00234@mail.ru>
2022-02-08 20:30:29 +0600

    change sprites for person; create idle and half of walk animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,23 +4,30 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 1.5f;
+    
+    private Animator _playerAnimator;
 
     void Start()
     {
-        
+        _playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
         UpdateMovement();
+        UpdateAnimation();
     }
 
     private void UpdateMovement()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+    	float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontal, vertical, 0f);
         transform.Translate(direction * _speed * Time.deltaTime);
     }
+    
+    private void UpdateAnimation()
+    {
+    }    
 }
