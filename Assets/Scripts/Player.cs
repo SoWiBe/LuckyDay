63c2e166df02a3b0f35f1340ac8d4ff56e419286
AntonBrevnov <brevnov-00234@mail.ru>
2022-02-09 08:15:20 +0600

    improve walking animation; implement flipping a player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,17 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 1.5f;
-    
+
+    private bool _isWalking = false;
+
     private Animator _playerAnimator;
 
-    void Start()
+    private void Start()
     {
         _playerAnimator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
         UpdateMovement();
         UpdateAnimation();
@@ -20,14 +22,33 @@ public class Player : MonoBehaviour
 
     private void UpdateMovement()
     {
-    	float horizontal = Input.GetAxis("Horizontal");
+        float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        CheckToFlip(horizontal);
+        _isWalking = horizontal > 0 || horizontal < 0 || vertical > 0 || vertical < 0;
 
         Vector3 direction = new Vector3(horizontal, vertical, 0f);
         transform.Translate(direction * _speed * Time.deltaTime);
     }
-    
+
     private void UpdateAnimation()
     {
-    }    
+        if (_isWalking)
+            _playerAnimator.SetBool("isWalking", true);
+        else
+            _playerAnimator.SetBool("isWalking", false);
+    }
+
+    public void CheckToFlip(float horizontal)
+    {
+        Vector3 localScale = transform.localScale;
+
+        if (horizontal < 0 && localScale.x > 0)
+            localScale.x *= -1;
+        if (horizontal > 0 && localScale.x < 0)
+            localScale.x *= -1;
+
+        transform.localScale = localScale;
+    }
 }
