af6f9813619aebe5bf8b3aaf324e34b9fc948728
DorzhuSergek <zairovd340@gmail.com>
2022-02-24 22:56:38 +0600

    start work with inventory

---
+++ Assets/Scripts/Items/AssetItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Item")]
+class AssetItem : IItem
+{
+    public string Name => throw new NotImplementedException();
+
+    public Texture2D UIICon => throw new NotImplementedException();
+
+    [SerializeField] private string name;
+    [SerializeField] private Texture2D _uiIcon;
+}
+
---
+++ Assets/Scripts/Items/IItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IItem
+{
+    string Name { get; }
+    Texture2D UIICon { get; }
+    
+}
